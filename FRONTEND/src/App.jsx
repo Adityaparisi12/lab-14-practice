c234a62aeed64f464e5d34c5620a6ba00be1308d
Aditya <adityaparisi12@gmail.com>
2025-11-05 19:02:29 +0530

    updated files

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import CompTracking from "./comptracking/CompTracking";
+
+function App() {
+
+  return (
+    <>
+      <CompTracking />
+    </>
+  );
+}
+
+export default App;
---
+++ FRONTEND/src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    url: "http://localhost:6645"
+};
+
+export default config;
